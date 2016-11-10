76da6b985fcab96ca3b3c524dcc73bb28bfa9e44
Adam Romines <adam.romines@gmail.com>
2016-11-10 14:05:58 -0700

    Adds some filtering markup

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,6 +2,17 @@
   <div class="list">
     <span class="header">Available Shifts</span>
     <button @click="reverseList">Reverse</button>
+    <span class="days">
+
+    <button @click="setDayFilter" class="dayOfWeek">S</button>
+    <button @click="setDayFilter" class="dayOfWeek">M</button>
+    <button @click="setDayFilter" class="dayOfWeek">T</button>
+    <button @click="setDayFilter" class="dayOfWeek">W</button>
+    <button @click="setDayFilter" class="dayOfWeek">R</button>
+    <button @click="setDayFilter" class="dayOfWeek">F</button>
+    <button @click="setDayFilter" class="dayOfWeek">S</button>
+
+    </span>
     <ul>
       <coaster v-for="coaster in filteredCoasters" :coaster="coaster" as:="'LIST'"></coaster>
     </ul>
@@ -40,7 +51,14 @@ export default {
     reverseList () {
       console.log(this.coasters);
       this.order = (this.order === 'ASC') ? 'DESC' : 'ASC'
+    },
+    setDayFilter (event) {
+        let getElemIndex = (node) => {
+
+        }
+        console.log(event.target.innerText);
     }
+
   },
   components: {
     Coaster
