1be89bc5d58695ab2377f6369ece18cd8713b9c1
Adam Romines <adam.romines@gmail.com>
2016-12-27 11:54:08 -0700

    Changes default ref

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import { firebase, moment } from '../libs'
 Vue.use(Vuex)
 
 const db = firebase.database()
-const coastersRef = db.ref('data/coasters')
+const coastersRef = db.ref('data/coasters').orderByChild('date').startAt('2016-01-02')
 
 const store = new Vuex.Store({
   state: {
