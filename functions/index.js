3434cd9deb5437e38506a7a4cda8078c616f604f
Adam Romines <adam.romines@gmail.com>
2017-04-23 13:56:41 -0600

    Restructure and attempt to get rid of cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,10 @@
 'use strict';
-
-const functions = require('firebase-functions');
-const coasterFanout = require('./coasterFanout.js');
-const pickupNotifications = require('./pickupNotifications.js')
-
-exports.generateThumbnail = require('./generateThumbnail.js').generateThumbnail;
-
-exports.fanoutAndNotifications = functions.database.ref('/data/coasters/{coasterId}')
+exports.generateThumbnail = functions.database.ref('/data/something/that/will/never/be/hit')
   .onWrite(event => {
-    // On coaster write, write updated coaster to /posted and /holding of
-    // all posters in history, and currently hodling user respectively
+		return
+  });
 
-    const coaster = event.data.val();
-    const historyKeys = Object.keys(coaster.history);
-    const latestPickup = coaster.history[historyKeys[historyKeys.length -1]];
-    const userKey = pickupNotifications.getUserToNofify(latestPickup)
-    const notieKey = root.child(`/users/${userKey}/notifications`).push().key
-
-    let updates = coasterFanout.getCoasterFanout(coaster);
-
-    // then add notifications for holding user to updates object
-    //
-    updates = pickupNotifications.getNotieUpdates(coaster, userKey, notieKey, latestPickup, updates);
-
-    // get root reference and write all updates
-    //
-    let rootRef = event.data.ref.parent.parent.parent;
-    let dataRef = rootRef.child('data')
-    dataRef.update(updates);
-
+exports.fanoutAndNotifications = functions.database.ref('/data/something/that/will/never/be/hit')
+  .onWrite(event => {
+		return
   });
