3f6768f5190904ebfef72d35ee20c1d3565de833
Adam Romines <adam.romines@gmail.com>
2017-05-08 23:35:09 -0600

    Adds dbURL swithc

--- utilities/firebase.js
+++ utilities/firebase.js
@@ -4,6 +4,7 @@ const env = process.env.NODE_ENV;
 const development = require("./fBServiceAccountKey_admin_dev.json");
 const production = require("./fBServiceAccountKey_admin.json");
 const environment = process.env.NODE_ENV === 'production' ? production : development
+const databaseURL = process.env.NODE_ENV === 'production' ? 'https://srb-coasters.firebaseio.com' : 'https://srb-coasters-dev.firebaseio.com'
 
 console.log(`. . . NODE_ENV is ${process.env.NODE_ENV} . . .`);
 
