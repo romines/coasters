04ab0a87a3b8f80035ede5a96b6ccbc99c9ac732
Adam Romines <adam.romines@gmail.com>
2017-05-08 23:58:06 -0600

    Fixes bug in fb initialize config object

--- utilities/firebase.js
+++ utilities/firebase.js
@@ -5,12 +5,13 @@ const development = require("./fBServiceAccountKey_admin_dev.json");
 const production = require("./fBServiceAccountKey_admin.json");
 const environment = process.env.NODE_ENV === 'production' ? production : development
 const databaseURL = process.env.NODE_ENV === 'production' ? 'https://srb-coasters.firebaseio.com' : 'https://srb-coasters-dev.firebaseio.com'
+const test = process.env.NODE_ENV === 'production'
 
-console.log(`. . . NODE_ENV is ${process.env.NODE_ENV} . . .`);
+console.log(`. . . NODE_ENV is equal to 'production': ${test} . . .`);
 
 admin.initializeApp({
   credential: admin.credential.cert(environment),
-  databaseURL: "https://srb-coasters-dev.firebaseio.com"
+  databaseURL
 });
 
 module.exports = admin.database();
