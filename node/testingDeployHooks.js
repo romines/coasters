7b7364826dc257bb6df89975039df7483d4f1892
Adam Romines <adam.romines@gmail.com>
2018-03-31 16:17:16 -0600

    Testing deploy hooks 5

--- node/testingDeployHooks.js
+++ node/testingDeployHooks.js
@@ -1,2 +1,34 @@
 /* eslint-env node */
-console.log('Hello from testingDeployHook.js');
+'use strict'
+
+const admin = require("firebase-admin");
+const environment = 'production';
+
+const serviceAccountKey = {
+  development : require("./fBServiceAccountKey_admin_dev.json"),
+  staging : require("./fBServiceAccountKey_admin_staging.json"),
+  production : require("./fBServiceAccountKey_admin.json")
+};
+const databaseURL = {
+  development : 'https://srb-coasters-dev.firebaseio.com',
+  staging : 'https://srb-coasters-staging.firebaseio.com',
+  production : 'https://srb-coasters.firebaseio.com'
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey[environment]),
+  databaseURL: databaseURL[environment],
+  storageBucket: "srb-coasters.appspot.com"
+});
+
+console.log(`Firebase connection established to ${environment} . . .`);
+
+const db = admin.database();
+const root = db.ref('data')
+root.child(`/coasters`).on('child_changed', (refData) => {
+  console.log('tDH says: there was a change to a coaster . . .');
+})
+
+process.on('exit', function(code) {
+  return console.log(`tDH exiting with code ${code}`);
+});
